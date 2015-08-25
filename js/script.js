56d861939558e79be6aa484f4abd1bb653ea2f00
kadehill <khill@fullsail.edu>
2015-08-25 17:25:24 -0400

    Defining Variables, Prompt and Function

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/** Kade Hill
+ * August 25, 2015
+ * SDI-1508
+ * Functions Assignment
+ */
+
+
+// variables
+
+
+//Prompts
+var lottery = prompt("Do you want to play the Florida Lottery or Powerball?");
+
+
+//My Functions
+function myLotto(){
+
+
+
+}
+
+
+// Main code that calls functions
+myLotto();
