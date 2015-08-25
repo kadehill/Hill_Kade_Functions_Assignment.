ad73c4d3045a13674a47f9ba23a75895b791b458
kadehill <khill@fullsail.edu>
2015-08-25 19:41:24 -0400

    Random Numbers

--- js/script.js
+++ js/script.js
@@ -6,33 +6,51 @@
 
 
 // variables
-var lottery = prompt("Do you want to play the Florida Lottery or Powerball?");
-
-
-
-
-
+var floridaQP;
+var lottery;
+//var lottery = prompt("Do you want to play the Florida Lottery or Powerball?");
 
 
 //My Functions
-function myLotto(florida) {
+function myLotto(lotto) {
 
     if(lottery != "Florida Lottery") {
-        console.log("You are playning the Powerball lottery.");
-    } else {
-        console.log("You are playing the Florida Lottery.");
+        console.log("Good Luck! You are playing the Powerball Lottery.");
+    }   else {
+        console.log("Good Luck! You are playing the Florida Lottery.");
     }
 
 
+    var chances = 1;
+
+    while (lotto === ""){
+        lotto = prompt("Please select a lotto game.");
+
+        chances++
+
+        if (chances === 5) {
+            console.log("Sorry, you have exceeded your chances to select a lotto game. Please play responsibly.");
+
+            break;
+        }
+    }
+            return lotto;
+
 }
 
+//Function to generate Florida Lotto numbers
+function floridaLotto(){
 
+        floridaQP = Math.random() * (53 - 1) + 1;
+        floridaQP = Math.round(floridaQP);
+        console.log(floridaQP);
 
-
-
+}
+//functions
 
 // Main code that calls my functions
-myLotto(lottery);
+//myLotto(lottery);
+floridaLotto();
     //var floridaLotto;
 
     //floridaLotto = Math.random() * (53 - 1) + 1;
