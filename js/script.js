c796ad54989609ef6dab3006818de05ef443c8f5
kadehill <khill@fullsail.edu>
2015-08-26 19:33:01 -0400

    Powerball Quick Pick Numbers

--- js/script.js
+++ js/script.js
@@ -8,19 +8,29 @@
 // variables
 var lottery;
 var floridaQP = [];
+var powerballQP = [];
+var powerB = [];
 
 
 var lottery = prompt("Do you want to play the Florida Lottery or Powerball?");
 
 
-//My Functions
-function myLotto(lotto) {
-
     if(lottery != "Florida Lottery") {
         console.log("Good Luck! You are playing the Powerball Lottery.");
-    }   else {
+        myLotto(lottery);
+        powerballLotto(59, 1, 5);
+        powerball(35,1,1);
+
+}   else {
         console.log("Good Luck! You are playing the Florida Lottery.");
-    }
+        floridaLotto(53, 1, 6);
+}
+
+
+
+//Function to prompt user to select a lotto game.
+function myLotto(lotto) {
+
 
 
     var chances = 1;
@@ -48,13 +58,39 @@ function floridaLotto(max, min, num){
         }
     console.log("Your Florida lottery quick pick numbers are " + floridaQP);
 
-    return floridaLotto;
 }
+
+
+
+//Function to generate Powerball quick pick numbers lotto numbers.
+function powerballLotto(max, min, num) {
+    for (var i = 0; i < num; i++) {
+        powerballQP[i] = Math.random() * (max - min) + min;
+        powerballQP[i] = Math.round(powerballQP[i]);
+    }
+    console.log("Your Powerball lottery quick pick numbers are " + powerballQP);
+}
+
+//Function to generate the single power ball.
+function powerball(max, min, num){
+    for (var i = 0; i < num; i++) {
+        powerB[i] = Math.random() * (max - min) + min;
+        powerB[i] = Math.round(powerB[i]);
+        console.log("Your Powerball is " + powerB);
+    }
+}
+
+
+
+
+
+
+
 //functions
 
 // Main code that calls my functions
- myLotto(lottery);
- floridaLotto(53, 1, 6);
+
+
 
     //var floridaLotto;
 
