2ee4b7fd4ae7f63e0c280800b365e0c5f2608ca2
kadehill <khill@fullsail.edu>
2015-08-26 21:07:52 -0400

    Notes

--- js/script.js
+++ js/script.js
@@ -7,30 +7,30 @@
 
 // variables
 
-var floridaQP = [];
-var powerballQP = [];
-var powerB = [];
+var floridaQP = []; //defining the array
+var powerballQP = []; //defining the array
+var powerB = []; //defining the array
 
+//Prompts
+var lottery = prompt("Do you want to play the Florida Lottery or Powerball?"); ///Prompt that forces user to select a specific lottery game.
+myLotto(lottery); //calling the function
 
-var lottery = prompt("Do you want to play the Florida Lottery or Powerball?");
-myLotto(lottery);
+    if(lottery != "Florida Lottery") {    //if the user's input is not Florida Lottery
+            if(lottery === "") {   //If the user leaves the prompt blank
+                console.log("Try again later."); //console output to user.
 
-    if(lottery != "Florida Lottery") {
-            if(lottery === ""){
-                console.log("Try again later.");
-
-            }else {
+            }if(lottery != "Powerball") { //if the user's input is not Powerball
+                console.log("You've entered an invalid lottery game. Please enter Powerball or Florida Lottery."); //the console advises user to input either Powerball or Florida Lottery.
+            }else { // if the user inputs Powerball, console wishes them good luck and then confirms user's selection.
                 console.log("Good Luck! You are playing the Powerball Lottery.");
 
-                powerballLotto(59, 1, 5);
-                powerball(35, 1, 1);
+                powerballLotto(59, 1, 5); //calling the powerball lottery quick pick function
+                powerball(35, 1, 1); //calling the main power ball function
             }
 }   else {
-        console.log("Good Luck! You are playing the Florida Lottery.");
-        floridaLotto(53, 1, 6);
+        console.log("Good Luck! You are playing the Florida Lottery."); //console confirms that user is playing the Florida Lottery.
+        floridaLotto(53, 1, 6); //calling the floridaLotto function.
 }
-
-
 
 
 //Function to prompt user to select a lotto game.
@@ -38,31 +38,31 @@ function myLotto(lotto) {
 
 
 
-    var chances = 1;
+    var chances = 1; //defining variable
 
-    while (lotto === ""){
-        lotto = prompt("Please select a lotto game.");
+    while (lotto === ""){ //as long as the user leaves prompt blank, a reminder message will appear.
+        lotto = prompt("Please select a lotto game."); //console message to remind user to select their desired lotto game.
 
-        chances++
+        chances++   //code to count number of chances
 
-        if (chances === 5) {
-            console.log("Sorry, you have exceeded your chances to select a lotto game. Please play responsibly.");
+        if (chances === 5) { //number of chances user has to input correct lottery name
+            console.log("Sorry, you have exceeded your chances to select a lotto game. Please play responsibly."); //console outputs sorry message once user goes over 5 chances.
 
-            break;
+            break; //breaks the loop
         }
     }
-            return lotto;
+            return lotto; //returns to lotto.
 
 }
 
 //Function to generate Florida Lotto numbers
 function floridaLotto(max, min, num){
         for (var i = 0; i < num; i++){
-            floridaQP[i] = Math.random() * (max - min) + min;
-            floridaQP[i] = Math.round(floridaQP[i]);
+            floridaQP[i] = Math.random() * (max - min) + min; //math object to select a random number
+            floridaQP[i] = Math.round(floridaQP[i]); //math object to round number to nearest whole number
         }
 
-    console.log("Your Florida lottery quick pick numbers are " + floridaQP);
+    console.log("Your Florida lottery quick pick numbers are " + floridaQP); //console outputs the Florida Lottery quick pick numbers.
 
 }
 
@@ -71,39 +71,36 @@ function floridaLotto(max, min, num){
 //Function to generate Powerball quick pick numbers lotto numbers.
 function powerballLotto(max, min, num) {
     for (var i = 0; i < num; i++) {
-        powerballQP[i] = Math.random() * (max - min) + min;
-        powerballQP[i] = Math.round(powerballQP[i]);
+        powerballQP[i] = Math.random() * (max - min) + min; //math object to select a random number
+        powerballQP[i] = Math.round(powerballQP[i]); //math object to round number to nearest whole number
     }
-    console.log("Your Powerball lottery quick pick numbers are " + powerballQP);
+    console.log("Your Powerball lottery quick pick numbers are " + powerballQP); //console outputs the Powerball quick pick numbers.
 }
 
 //Function to generate the single power ball.
 function powerball(max, min, num){
     for (var i = 0; i < num; i++) {
-        powerB[i] = Math.random() * (max - min) + min;
-        powerB[i] = Math.round(powerB[i]);
+        powerB[i] = Math.random() * (max - min) + min; //math object to select a random number
+        powerB[i] = Math.round(powerB[i]); //math object to round number to nearest whole number
 
     }
-    console.log("Your Powerball is " + powerB);
+    console.log("Your Powerball is " + powerB); //console outputs the Powerball number.
 }
 
 
+//FL Lottery Numbers: 49,48,50,18,53,38,
+//                     21,22,24,5,45,33
+//                     21,16,25,40,27,23
+
+
+//Powerball Lottery Numbers: 30,9,2,24,29 and the PB is 8
+//                           6,5,9,20,46 and the PB is 31
+//                           26,58,20,9,27 and the PB is 24
 
 
 
 
 
-
-//functions
-
-// Main code that calls my functions
-
-
-
-    //var floridaLotto;
-
-    //floridaLotto = Math.random() * (53 - 1) + 1;
-    //console.log(floridaLotto);
 
 
 
